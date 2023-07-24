2cc90fcae1bece34262659a5de1c87f9331a93d6
Simon Dutka <SimonDutkaWork@gmail.com>
2023-07-25 00:50:14 +0200

    Added function to end game if player finished it

--- modules/gameplay.js
+++ modules/gameplay.js
@@ -1,6 +1,8 @@
 import { readFile } from 'node:fs';
 import { select } from '@inquirer/prompts';
 import title from './title.js';
+import figlet from 'figlet';
+import center from 'center-align';
 
 // While game is on this value is true
 let gameStatus = true;
@@ -31,6 +33,24 @@ const gameplay = () => {
             let secondChoiceValue =
                 story.choices[story.messages[currentMessageId].secondChoice]
                     .nextMessageId;
+
+            // End game if it was last message
+            // Todo: Change number to last message ID
+            if (story.messages[currentMessageId].id === 100) {
+                gameStatus = false;
+                console.clear();
+                console.log('End message');
+
+                figlet('The End', (err, text) => {
+                    if (err) {
+                        console.log('Something went wrong...');
+                        return;
+                    }
+                    console.log(center(text, process.stdout.columns));
+                });
+
+                process.exit();
+            }
 
             move = await select({
                 message: `${story.messages[currentMessageId].message}`,
