033c57bbe44a600f31ebca3102d9eb576640ce16
Simon Dutka <SimonDutkaWork@gmail.com>
2023-07-24 21:44:26 +0200

    Added basic physics of gameplay

--- modules/gameplay.js
+++ modules/gameplay.js
@@ -1,7 +1,39 @@
 import { readFile } from 'node:fs';
+import { select } from '@inquirer/prompts';
 
-readFile('./story.json', 'utf-8', (err, data) => {
-    if (err) throw err;
-    let story = JSON.parse(data);
-    console.log(json);
-});
+// While game is on this value is true
+let gameStatus = true;
+
+const gameplay = () => {
+    readFile('./story.json', 'utf-8', async (err, data) => {
+        if (err) throw err;
+        let story = JSON.parse(data);
+
+        let currentMessageId, move;
+
+        while (gameStatus) {
+            currentMessageId === undefined
+                ? (currentMessageId = 0)
+                : (currentMessageId = move);
+
+            let firstChoice = story.messages[currentMessageId].firstChoice;
+            let secondChoice = story.messages[currentMessageId].secondChoice;
+
+            move = await select({
+                message: `${story.messages[currentMessageId].message}`,
+                choices: [
+                    {
+                        name: `${story.choices[firstChoice].name}`,
+                        value: `${story.choices[firstChoice].nextMessageId}`,
+                    },
+                    {
+                        name: `${story.choices[secondChoice].name}`,
+                        value: `${story.choices[secondChoice].nextMessageId}`,
+                    },
+                ],
+            });
+        }
+    });
+};
+
+export default gameplay;
